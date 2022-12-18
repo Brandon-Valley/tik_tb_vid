f64a25fa548ccfba99650725ee8ad837bb084c93
Brandon Valley <bavvh8@mst.edu>
2022-12-18 03:44:14 -0600

    custom_edit_top_vid() init works

--- src/make_tb_vid.py
+++ src/make_tb_vid.py
@@ -11,6 +11,7 @@ BIG_DATA_WORKING_DIR_PATH = os.path.join(BIG_DATA_DIR_PATH, "working")
 TEST_OUT_MP4_PATH = os.path.join(BIG_DATA_DIR_PATH, "test_out_vids", "test_out.mp4")
 
 SCALED_TOP_VID_PATH = os.path.join(BIG_DATA_WORKING_DIR_PATH, "scaled_top_vid.mp4")
+CUSTOM_EDITED_TOP_VID_PATH = os.path.join(BIG_DATA_WORKING_DIR_PATH, "custom_edited_top_vid.mp4")
 CUSTOM_EDITED_BOTTOM_VID_PATH = os.path.join(BIG_DATA_WORKING_DIR_PATH, "custom_edited_bottom_vid.mp4")
 SCALED_BOTTOM_VID_PATH = os.path.join(BIG_DATA_WORKING_DIR_PATH, "scaled_bottom_vid_after_custom_edit.mp4")
 TIME_TRIMMED_BOTTOM_VID_PATH = os.path.join(BIG_DATA_WORKING_DIR_PATH, "time_trimmed_bottom_vid.mp4")
@@ -81,16 +82,52 @@ def _trim_sides_of_vid_to_match_aspect_ratio(vid_dim_tup_to_match_aspect_ratio, 
                  y = 0,
                  in_vid_path = in_vid_path, out_vid_path = out_vid_path)
 
+def _trim_sides_of_vid_by_percent(trim_percent, in_vid_path, out_vid_path):
+    """ 
+        Good for trimming non-important sides of shows like Family Guy 
+        - Hacky work-around, proper machine vision to identify characters/important things, signs,
+          etc. is the true solution to this.
+        - trim_percent =  10, 20, 30, etc.
+    """
+    in_vid_dim_tup = veu.get_vid_dims(in_vid_path)
+    in_vid_w = in_vid_dim_tup[0]
+    in_vid_h = in_vid_dim_tup[1]
 
-def custom_edit_bottom_vid(vid_dim_tup_to_match_aspect_ratio, in_vid_path, out_vid_path, custom_edit_bottom_vid_method_str):
-    if custom_edit_bottom_vid_method_str == "trim_sides":
+    # num_pixels_to_trim_total = int(in_vid_w / trim_percent)
+    num_pixels_to_keep_total = in_vid_w - int(in_vid_w / trim_percent)
+    # print(f"{num_pixels_to_trim_total=}")
+    print(f"{num_pixels_to_keep_total=}")
+    num_pixels_to_trim_from_both_sides = int(int(in_vid_w / trim_percent) / 2)
+    print(f"{num_pixels_to_trim_from_both_sides=}")
+
+    # veu.crop_vid(w = num_pixels_to_trim_total,
+    veu.crop_vid(w = num_pixels_to_keep_total,
+                 h = in_vid_h,
+                 x = num_pixels_to_trim_from_both_sides,
+                 y = 0,
+                 in_vid_path = in_vid_path, out_vid_path = out_vid_path)
+
+def custom_edit_top_vid(in_vid_path, out_vid_path, custom_edit_vid_method_str, trim_sides_percent):
+    if custom_edit_vid_method_str == "None":
+        raise Exception("ERROR: not implemented yet")
+    if custom_edit_vid_method_str == "trim_sides_by_percent":
+        _trim_sides_of_vid_by_percent(trim_sides_percent, in_vid_path, out_vid_path)
+    else:
+        raise Exception(f"ERROR: invalid {custom_edit_vid_method_str=}")
+
+def custom_edit_bottom_vid(vid_dim_tup_to_match_aspect_ratio, in_vid_path, out_vid_path, custom_edit_vid_method_str):
+    if custom_edit_vid_method_str == "trim_sides":
         _trim_sides_of_vid_to_match_aspect_ratio(vid_dim_tup_to_match_aspect_ratio, in_vid_path, out_vid_path)
     else:
-        raise Exception(f"ERROR: invalid {custom_edit_bottom_vid_method_str=}")
+        raise Exception(f"ERROR: invalid {custom_edit_vid_method_str=}")
 
 
 
-def make_tb_vid(vid_dim_tup, top_vid_path, bottom_vid_path, use_audio_from_str = "top", time_trim_bottom_vid_method_str = "from_rand_start", custom_edit_bottom_vid_method_str = "trim_sides"):
+def make_tb_vid(vid_dim_tup, top_vid_path, bottom_vid_path, use_audio_from_str = "top", 
+                time_trim_bottom_vid_method_str = "from_rand_start", 
+                custom_edit_bottom_vid_method_str = "trim_sides",
+                custom_edit_top_vid_method_str = "trim_sides_by_percent",
+                trim_top_vid_sides_percent = 10):
     """ - Zoom top vid in or out to fit vid_dim_tup,
         - Do same for bottom vid with remaining dims?
         - Assume top_vid is already the length you want
@@ -101,13 +138,19 @@ def make_tb_vid(vid_dim_tup, top_vid_path, bottom_vid_path, use_audio_from_str =
             - LOOP???
             - MORE???
     """
-
     # veu.remove_watermark(top_vid_path, "C:\\Users\\Brandon\\Documents\\Personal_Projects\\tik_tb_vid_big_data\\working\\removed_watermark_test.mp4")
 
-    new_top_vid_dim_tup = get_w_matched_new_vid_dims(vid_dim_tup, top_vid_path)
+    # Perform custom edit to top vid
+    # - This can be different depending on custom_edit_top_vid_method_str to best match the type of vid on top
+    # - This is done before final scaling (making top vid bigger or smaller) because this edit might not be
+    #   pixel-perfect and the final top scale will stretch the vid a tiny bit if needed to fit pixels
+    custom_edit_top_vid(top_vid_path, CUSTOM_EDITED_TOP_VID_PATH, custom_edit_top_vid_method_str, trim_top_vid_sides_percent)
+
+    # new_top_vid_dim_tup = get_w_matched_new_vid_dims(vid_dim_tup, top_vid_path)
+    new_top_vid_dim_tup = get_w_matched_new_vid_dims(vid_dim_tup, CUSTOM_EDITED_TOP_VID_PATH)
     print(f"{new_top_vid_dim_tup=}")
 
-    # veu.scale_vid(new_top_vid_dim_tup, top_vid_path, SCALED_TOP_VID_PATH) # PUT BACK!!!!!!!!!!!
+    veu.scale_vid(new_top_vid_dim_tup, top_vid_path, SCALED_TOP_VID_PATH) # PUT BACK!!!!!!!!!!!
 
     # scale_vid() can change h by 1 pixel, get fresh dims to be safe
     scaled_top_vid_dims_tup = veu.get_vid_dims(SCALED_TOP_VID_PATH)
