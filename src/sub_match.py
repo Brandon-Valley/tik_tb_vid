b1c47f5816d430a854266db57f2f7dd965d64f6a
Brandon Valley <bavvh8@mst.edu>
2023-01-06 06:28:51 -0600

    still testing vv, _get_and_check_real_and_auto_subs() init

--- src/sub_match.py
+++ src/sub_match.py
@@ -22,16 +22,30 @@ FUZZ_STR_DELIM = ' '
 #     # [Match(start=3, end=9, dist=1, matched="PATERN")]
 #     return match
 
+def _get_and_check_real_and_auto_subs(real_sub_file_path, auto_sub_file_path):
+    real_subs = pysubs2.load(real_sub_file_path, encoding="utf-8")
+    auto_subs = pysubs2.load(auto_sub_file_path, encoding="utf-8")
+
+    if len(real_subs) == 0:
+        raise Exception(f"ERROR: {len(real_subs)=} - I assume this is a problem?")
+    if len(auto_subs) == 0:
+        raise Exception(f"ERROR: {len(auto_subs)=} - I assume this is a problem?")
+    if len(auto_subs) > len(real_subs):
+        raise Exception(f"ERROR: {len(auto_subs)=} > {len(real_subs)=} - This should never be possible, maybe sub formats are weird? {real_sub_file_path=}, {auto_sub_file_path=}")
+    
+    return real_subs, auto_subs
+
 def _compare_sub_slots(real_subs, auto_subs, sub_slot_offset):
+    print(f"..in _compare_sub_slots() - {sub_slot_offset=}")
     sub_slot_score = 0
     best_auto_sub_line_match_index = False
     best_auto_sub_line_match_score = 0
 
     for auto_sub_line_num, auto_sub_line in enumerate(auto_subs):
         real_sub_line = real_subs[auto_sub_line_num + sub_slot_offset]
-        print(f"{auto_sub_line_num=}")
-        print(f"{auto_sub_line.text=}")
-        print(f"{real_sub_line.text=}")
+        print(f"....{auto_sub_line_num=}")
+        print(f"....{auto_sub_line.text=}")
+        print(f"....{real_sub_line.text=}")
 
         auto_sub_line_match_score = fuzz.ratio(auto_sub_line.text, real_sub_line.text)
         sub_slot_score += auto_sub_line_match_score
@@ -47,15 +61,16 @@ def trim_and_re_time_real_sub_file_from_auto_subs(real_sub_file_path, auto_sub_f
     fsu.delete_if_exists(out_sub_path)
     Path(out_sub_path).parent.mkdir(parents=True, exist_ok=True)
 
-    real_subs = pysubs2.load(real_sub_file_path, encoding="utf-8")
-    auto_subs = pysubs2.load(auto_sub_file_path, encoding="utf-8")
+    real_subs, auto_subs = _get_and_check_real_and_auto_subs(real_sub_file_path, auto_sub_file_path)
+    # real_subs = pysubs2.load(real_sub_file_path, encoding="utf-8")
+    # auto_subs = pysubs2.load(auto_sub_file_path, encoding="utf-8")
 
-    if len(real_subs) == 0:
-        raise Exception(f"ERROR: {len(real_subs)=} - I assume this is a problem?")
-    if len(auto_subs) == 0:
-        raise Exception(f"ERROR: {len(auto_subs)=} - I assume this is a problem?")
-    if len(auto_subs) > len(real_subs):
-        raise Exception(f"ERROR: {len(auto_subs)=} > {len(real_subs)=} - This should never be possible, maybe sub formats are weird? {real_sub_file_path=}, {auto_sub_file_path=}")
+    # if len(real_subs) == 0:
+    #     raise Exception(f"ERROR: {len(real_subs)=} - I assume this is a problem?")
+    # if len(auto_subs) == 0:
+    #     raise Exception(f"ERROR: {len(auto_subs)=} - I assume this is a problem?")
+    # if len(auto_subs) > len(real_subs):
+    #     raise Exception(f"ERROR: {len(auto_subs)=} > {len(real_subs)=} - This should never be possible, maybe sub formats are weird? {real_sub_file_path=}, {auto_sub_file_path=}")
 
     possible_sub_slots = len(real_subs) - len(auto_subs)
     print(f"{possible_sub_slots=}")
@@ -63,8 +78,8 @@ def trim_and_re_time_real_sub_file_from_auto_subs(real_sub_file_path, auto_sub_f
     for sub_slot_offset in range(possible_sub_slots):
         print(f"{sub_slot_offset=}")
         sub_slot_score, best_auto_sub_line_match_index = _compare_sub_slots(real_subs, auto_subs, sub_slot_offset)
-        print(f"..{sub_slot_score=}")
-        print(f"..{best_auto_sub_line_match_index=}")
+        print(f"......{sub_slot_score=}")
+        print(f"......{best_auto_sub_line_match_index=}")
 
         if best_auto_sub_line_match_index == False:
             raise Exception(f"ERROR: {best_auto_sub_line_match_index=}, this means maybe some subs are empty or something else happened?")
