81f4c69cb5c97a17d702a8246ce11588ebbe416f
Brandon Valley <bavvh8@mst.edu>
2023-01-06 06:43:27 -0600

    gets to end of loop and finds "best match" still testing if this is correct

--- src/sub_match.py
+++ src/sub_match.py
@@ -43,9 +43,9 @@ def _compare_sub_slots(real_subs, auto_subs, sub_slot_offset):
 
     for auto_sub_line_num, auto_sub_line in enumerate(auto_subs):
         real_sub_line = real_subs[auto_sub_line_num + sub_slot_offset]
-        print(f"....{auto_sub_line_num=}")
-        print(f"....{auto_sub_line.text=}")
-        print(f"....{real_sub_line.text=}")
+        # print(f"....{auto_sub_line_num=}")
+        # print(f"....{auto_sub_line.text=}")
+        # print(f"....{real_sub_line.text=}")
 
         auto_sub_line_match_score = fuzz.ratio(auto_sub_line.text, real_sub_line.text)
         sub_slot_score += auto_sub_line_match_score
@@ -62,19 +62,14 @@ def trim_and_re_time_real_sub_file_from_auto_subs(real_sub_file_path, auto_sub_f
     Path(out_sub_path).parent.mkdir(parents=True, exist_ok=True)
 
     real_subs, auto_subs = _get_and_check_real_and_auto_subs(real_sub_file_path, auto_sub_file_path)
-    # real_subs = pysubs2.load(real_sub_file_path, encoding="utf-8")
-    # auto_subs = pysubs2.load(auto_sub_file_path, encoding="utf-8")
-
-    # if len(real_subs) == 0:
-    #     raise Exception(f"ERROR: {len(real_subs)=} - I assume this is a problem?")
-    # if len(auto_subs) == 0:
-    #     raise Exception(f"ERROR: {len(auto_subs)=} - I assume this is a problem?")
-    # if len(auto_subs) > len(real_subs):
-    #     raise Exception(f"ERROR: {len(auto_subs)=} > {len(real_subs)=} - This should never be possible, maybe sub formats are weird? {real_sub_file_path=}, {auto_sub_file_path=}")
 
     possible_sub_slots = len(real_subs) - len(auto_subs)
     print(f"{possible_sub_slots=}")
 
+    best_sub_slot_offset = 0
+    best_sub_slot_score = 0
+    best_auto_sub_line_match_index_for_best_sub_slot_offset = 0 # is this correct thing to do if auto_subs == real_subs?
+                                                                # any way len(auto_subs) == len(real_subs) but auto_subs != real_subs? # TODO test
     for sub_slot_offset in range(possible_sub_slots):
         print(f"{sub_slot_offset=}")
         sub_slot_score, best_auto_sub_line_match_index = _compare_sub_slots(real_subs, auto_subs, sub_slot_offset)
@@ -83,6 +78,23 @@ def trim_and_re_time_real_sub_file_from_auto_subs(real_sub_file_path, auto_sub_f
 
         if best_auto_sub_line_match_index == False:
             raise Exception(f"ERROR: {best_auto_sub_line_match_index=}, this means maybe some subs are empty or something else happened?")
+        
+        if sub_slot_score > best_sub_slot_score:
+            print(f"....new lead sub_slot found:")
+            best_sub_slot_offset = sub_slot_offset
+            best_sub_slot_score = sub_slot_score
+            best_auto_sub_line_match_index_for_best_sub_slot_offset = best_auto_sub_line_match_index
+            print(f"      - {best_sub_slot_offset=}")
+            print(f"      - {best_sub_slot_score=}")
+            print(f"      - {best_auto_sub_line_match_index_for_best_sub_slot_offset=}")
+    
+    print("Final best sub_slot for file:")
+    print(f"  - {best_sub_slot_offset=}")
+    print(f"  - {best_sub_slot_score=}")
+    print(f"  - {best_auto_sub_line_match_index_for_best_sub_slot_offset=}")
+    print(f"  - {auto_subs[best_auto_sub_line_match_index_for_best_sub_slot_offset].text=}")
+    print(f"  - {real_subs[best_sub_slot_offset + best_auto_sub_line_match_index_for_best_sub_slot_offset].text=}")
+
 
 
 
