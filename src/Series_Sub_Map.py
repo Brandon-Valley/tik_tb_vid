d940c96dc11d0e996a3713b7b61ad878a34e3e2e
Brandon Valley <bavvh8@mst.edu>
2023-01-08 08:49:21 -0600

    in middle, _load_dir__many_of_one_lang() init

--- src/Series_Sub_Map.py
+++ src/Series_Sub_Map.py
@@ -1,22 +1,61 @@
 from sms.file_system_utils import file_system_utils as fsu
-
+from pathlib import Path
 
 
 class Episode_Sub_Data:
-    def __init__(self):
-        pass
+    extra_metadata = None
 
+    def __init__(self, episode_subs_dir_path, season_num, episode_num, load_method_str = "many_of_one_lang"):
+        self.episode_subs_dir_path = episode_subs_dir_path
+        self.season_num = season_num
+        self.episode_num = episode_num
+        self.load_method_str = load_method_str
 
+        if load_method_str == "many_of_one_lang":
+            self._load_dir__many_of_one_lang()
+        else:
+            raise Exception(f"ERROR: unknown {load_method_str=}")
+
+    def _load_dir__many_of_one_lang(self):
+        print("in _load_dir__many_of_one_lang()")
+        # exit()
 class Series_Sub_map():
-    ep_sub_data_l = []
+    ep_sub_data_ld = {}
 
 
     def __init__(self):
         print("in init")
         pass
 
-    def _load_lang__open_sub_lang_by_season_fg(self,in_dir_path, lang ):
+    def _load_lang__open_sub_lang_by_season_fg(self, in_dir_path, lang ):
         print("in _load_lang__open_sub_lang_by_season_fg()")
+
+        self.ep_sub_data_ld[lang] = []
+
+        lang_season_dir_path_l = fsu.get_dir_content_l(in_dir_path, "dir")
+        # print(f"{lang_season_dir_path_l=}")
+
+        for lang_season_dir_path in lang_season_dir_path_l:
+            # get season_num
+            season_dir_name = Path(lang_season_dir_path).stem
+            # print(f"{season_dir_name=}")
+            season_num = int(season_dir_name.split("s")[1])
+            print(f"{season_num=}")
+
+            ep_dir_path_l = fsu.get_dir_content_l(lang_season_dir_path, "dir")
+            for ep_dir_path in ep_dir_path_l:
+                
+                ep_num = int(Path(ep_dir_path).stem.split("episode ")[1])
+                print(f"{ep_num=}")
+
+                ep_sub_data = Episode_Sub_Data(episode_subs_dir_path = ep_dir_path,
+                                               season_num = season_num,
+                                               episode_num = ep_num,
+                                               load_method_str = "many_of_one_lang")
+
+
+
+
 
     def load_lang(self, in_dir_path, lang, load_style_str = "open_sub_lang_by_season_fg"):
 
