e4e641e6b95bed31a8d95cf2beb2d690f264ff67
Brandon Valley <bavvh8@mst.edu>
2023-01-15 09:18:33 -0600

    vv

--- src/get_real_episode_sub_data_from_auto_sub.py
+++ src/get_real_episode_sub_data_from_auto_sub.py
@@ -186,11 +186,24 @@ def _search_method__auto_sub_fuzz_len_based(auto_sub_path, auto_sub_fuzz_str, ss
     return fuzz_ratio, ep_sub_data, eval_str
 
 
-def _search_method__init_partial_fuzz(auto_sub_path, auto_sub_fuzz_str, ssm, lang):
+# def _search_method__init_partial_fuzz(auto_sub_path, auto_sub_fuzz_str, ssm, lang):
+#     fuzz_ratio_ep_sub_data_l_d_json_path = os.path.join(CLIPS_DATA_DIR_PATH, Path(auto_sub_path).name.split(".")[0][:70], Path(auto_sub_path).name.split(".")[0][:70] + "_lb_fresdl_d.json" ) #TMP
+
+#     print(f"Getting fuzz_ratio_ep_sub_data_l_d for {auto_sub_path=}...")
+#     fuzz_ratio_ep_sub_data_l_d = _get_fuzz_ratio_ep_sub_data_l_d(auto_sub_fuzz_str, ssm, lang, method_key=SEARCH_METHOD_KEY__INIT_PARTIAL_FUZZ)
+#     _write_fuzz_ratio_ep_sub_data_l_d_to_json(fuzz_ratio_ep_sub_data_l_d, fuzz_ratio_ep_sub_data_l_d_json_path)
+#     fuzz_ratio, ep_sub_data, eval_str = get_eval_of__fuzz_ratio_ep_sub_data_l_d(fuzz_ratio_ep_sub_data_l_d, ssm, lang, fuzz_ratio_ep_sub_data_l_d_json_path)
+
+#     # return fuzz_ratio, ep_sub_data, eval_str
+#     return fuzz_ratio, ep_sub_data, eval_str, fuzz_ratio_ep_sub_data_l_d
+ 
+
+
+def _search_and_log(auto_sub_path, auto_sub_fuzz_str, ssm, lang, method_key):
     fuzz_ratio_ep_sub_data_l_d_json_path = os.path.join(CLIPS_DATA_DIR_PATH, Path(auto_sub_path).name.split(".")[0][:70], Path(auto_sub_path).name.split(".")[0][:70] + "_lb_fresdl_d.json" ) #TMP
 
     print(f"Getting fuzz_ratio_ep_sub_data_l_d for {auto_sub_path=}...")
-    fuzz_ratio_ep_sub_data_l_d = _get_fuzz_ratio_ep_sub_data_l_d(auto_sub_fuzz_str, ssm, lang, method_key=SEARCH_METHOD_KEY__INIT_PARTIAL_FUZZ)
+    fuzz_ratio_ep_sub_data_l_d = _get_fuzz_ratio_ep_sub_data_l_d(auto_sub_fuzz_str, ssm, lang, method_key)
     _write_fuzz_ratio_ep_sub_data_l_d_to_json(fuzz_ratio_ep_sub_data_l_d, fuzz_ratio_ep_sub_data_l_d_json_path)
     fuzz_ratio, ep_sub_data, eval_str = get_eval_of__fuzz_ratio_ep_sub_data_l_d(fuzz_ratio_ep_sub_data_l_d, ssm, lang, fuzz_ratio_ep_sub_data_l_d_json_path)
 
@@ -208,12 +221,15 @@ def get_real_episode_sub_data_from_auto_sub(auto_sub_path, ssm, lang):
 
     auto_sub_fuzz_str = fc.get_fuzz_str_from_sub_path(auto_sub_path)
 
+    # Try to predict method that will find match fastest
+    # LATER for SEARCH_METHOD_KEY__AUTO_SUB_FUZZ_LEN_BASED, make it also give partial_fuzz_str_len
     search_method_key = _predict_search_method(auto_sub_fuzz_str, ssm, lang)
     print(f"init predicted {search_method_key=}")
 
+    # Default - Use the largest possible equal partial_fuzz_strs created for each episode during SSM.load_lang()
     if search_method_key == SEARCH_METHOD_KEY__INIT_PARTIAL_FUZZ:
-        # fuzz_ratio, ep_sub_data, eval_key = _search_method__init_partial_fuzz(auto_sub_path, auto_sub_fuzz_str, ssm, lang)
-        fuzz_ratio, ep_sub_data, eval_key, fuzz_ratio_ep_sub_data_l_d = _search_method__init_partial_fuzz(auto_sub_path, auto_sub_fuzz_str, ssm, lang)
+        # fuzz_ratio, ep_sub_data, eval_key, fuzz_ratio_ep_sub_data_l_d = _search_method__init_partial_fuzz(auto_sub_path, auto_sub_fuzz_str, ssm, lang)
+        fuzz_ratio, ep_sub_data, eval_key, fuzz_ratio_ep_sub_data_l_d = _search_and_log(auto_sub_path, auto_sub_fuzz_str, ssm, lang,method_key=SEARCH_METHOD_KEY__INIT_PARTIAL_FUZZ)
         print(f"{fuzz_ratio=}")
         print(f"{ep_sub_data=}")
         print(f"{eval_key=}")
@@ -280,6 +296,8 @@ def get_real_episode_sub_data_from_auto_sub(auto_sub_path, ssm, lang):
     print(f"{search_method_key=}")
     print(f"{partial_fuzz_str_len=}")
     raise Exception("SHOULD NEVER GET HERE")
+
+
 
 if __name__ == "__main__":
     import os.path as path
