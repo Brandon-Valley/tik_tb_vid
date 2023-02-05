da37dec36603d11df364d841ae674dbb89690612
Brandon Valley <bavvh8@mst.edu>
2023-02-05 09:49:45 -0600

    write_run_log_l__sorted_by__best_sub_diff_ratio__w_sorted__sub_diff_ratio_sub_path_l_d() works

--- src/subs/write_post_run_reports.py
+++ src/subs/write_post_run_reports.py
@@ -1,3 +1,4 @@
+import collections
 import statistics
 import time
 import os
@@ -25,6 +26,7 @@ import sub_diff_ratio_tools
 # SCRIPT_PARENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 POST_RUN_REPORT_JSON_PATH = join(cfg.INIT_MKVS_WORKING_DIR_PATH, "post_run_report.json")
 RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS_JSON_PATH = join(cfg.INIT_MKVS_WORKING_DIR_PATH, "run_log_l__sorted_by__best_sub_diff_ratio.json")
+RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS__W_SORTED__SUB_DIFF_RATIO_SUB_PATH_L_D_JSON_PATH = join(cfg.INIT_MKVS_WORKING_DIR_PATH, "run_log_l__sorted_by__best_sub_diff_ratio__w_sorted__sub_diff_ratio_sub_path_l_d.json")
 
 def write_run_log_l__sorted_by__best_sub_diff_ratio():
     def _get_best_sub_dir_ratio(clip_data_d):
@@ -47,11 +49,44 @@ def write_run_log_l__sorted_by__best_sub_diff_ratio():
     run_log_l = json_logger.read(cfg.RUN_LOG_JSON_PATH)
 
     sorted_run_log_l = sorted(run_log_l, key=lambda clip_data_d: _get_best_sub_dir_ratio(clip_data_d))
+    print(f"Writing {RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS_JSON_PATH}...")
     json_logger.write(sorted_run_log_l, RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS_JSON_PATH)
+
+def write_run_log_l__sorted_by__best_sub_diff_ratio__w_sorted__sub_diff_ratio_sub_path_l_d():
+    # def _get_best_sub_dir_ratio(clip_data_d):
+    #     # print(f"clip_data_d")
+    #     # pprint(clip_data_d)
+    #     # print(f"{len(clip_data_d)=}")
+
+    #     best_sub_diff_ratio = 9999
+
+    #     if "sub_diff_ratio_sub_path_l_d" in clip_data_d.keys():
+    #         sub_diff_ratio_sub_path_l_d = clip_data_d["sub_diff_ratio_sub_path_l_d"]
+    #         best_sub_diff_ratio = 999
+            
+    #         if len(sub_diff_ratio_sub_path_l_d) != 0:
+    #             best_sub_diff_ratio_str = min(sub_diff_ratio_sub_path_l_d.keys())
+    #             best_sub_path_l = sub_diff_ratio_sub_path_l_d[best_sub_diff_ratio_str]
+    #             best_sub_diff_ratio = float(best_sub_diff_ratio_str)
+    #     return best_sub_diff_ratio
+
+    sorted_run_log_l = json_logger.read(RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS_JSON_PATH)
+    new_log_l = []
+
+    for clip_data_d in sorted_run_log_l:
+        if "sub_diff_ratio_sub_path_l_d" in clip_data_d.keys():
+            # clip_data_d["sub_diff_ratio_sub_path_l_d"] = sorted(clip_data_d["sub_diff_ratio_sub_path_l_d"])
+            clip_data_d["sub_diff_ratio_sub_path_l_d"] = collections.OrderedDict(sorted(clip_data_d["sub_diff_ratio_sub_path_l_d"].items()))
+        new_log_l.append(clip_data_d)
+
+    # sorted_run_log_l = sorted(run_log_l, key=lambda clip_data_d: _get_best_sub_dir_ratio(clip_data_d))
+    print(f"Writing {RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS__W_SORTED__SUB_DIFF_RATIO_SUB_PATH_L_D_JSON_PATH}...")
+    json_logger.write(sorted_run_log_l, RUN_LOG_L__SORTED_BY__BEST_SUB_DIFF_RATIOS__W_SORTED__SUB_DIFF_RATIO_SUB_PATH_L_D_JSON_PATH)
 
 
 if __name__ == "__main__":
     import os.path as path
     print("Running " , path.abspath(__file__) , '...')
     write_run_log_l__sorted_by__best_sub_diff_ratio()
+    write_run_log_l__sorted_by__best_sub_diff_ratio__w_sorted__sub_diff_ratio_sub_path_l_d()
     print("End of Main") 
